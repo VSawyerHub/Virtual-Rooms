92e5b61d4d94b6e2962a1afd36cf84998a687661
victory <vsawyerdev@proton.me>
2026-08-08 13:36:33 -0300

    feat: add method to find room by code

--- src/main/java/com/virtualrooms/demo/RoomRepository.java
+++ src/main/java/com/virtualrooms/demo/RoomRepository.java
@@ -5,8 +5,11 @@ import org.springframework.stereotype.Repository;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface RoomRepository extends JpaRepository<Room, String> {
     List<Room> findByLastActivityAtBefore(LocalDateTime cutoff);
+
+    Optional<Room> findByCode(String code);
 }
